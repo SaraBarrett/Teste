9f3c08c5a3810ec69a6f73ce66184ccf36fc6776
Gonçalo Szabo Rio <goncaloszabo@gmail.com>
2025-02-24 15:11:52 +0000

    hello

--- FilamentTest/resources/views/welcome.blade.php
+++ FilamentTest/resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Hello, is it me you looking for ?</title>
+    <title>Hello, is it me you looking for ?</title>fdsfsdf
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
